fc641f775d864f197ce18896e75dadd7a7184138
Iain <iain@zora.co>
2022-10-18 00:09:35 -0400

    setup contract

--- script/Setup.s.sol
+++ script/Setup.s.sol
@@ -3,10 +3,24 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
+import {VRFNFTRandomDraw} from "../src/VRFNFTRandomDraw.sol";
+import {VRFNFTRandomDrawFactory} from "../src/VRFNFTRandomDrawFactory.sol";
+
 contract CounterScript is Script {
-    function setUp() public {}
+    address coordinatorAddress;
+
+    function setUp() public {
+        coordinatorAddress = vm.envAddress("CHAINLINK_COORDINATOR");
+    }
 
     function run() public {
         vm.broadcast();
+        VRFNFTRandomDraw drawImpl = VRFNFTRandomDraw(coordinatorAddress);
+        VRFNFTRandomDrawFactory factory = VRFNFTRandomDrawFactory(
+            address(drawImpl)
+        );
+
+        vm.stopBroadcast();
+        vm.label(address(factory), "factory");
     }
 }
